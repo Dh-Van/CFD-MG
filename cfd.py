804cb809b55d716e24b4434934789b4aea8df092
Dhvan <34456470+Dh-Van@users.noreply.github.com>
2024-02-13 20:55:53 -0500

    z

--- cfd.py
+++ cfd.py
@@ -2,6 +2,8 @@ import pygame
 import pygame_gui
 import random
 import numpy as np
+import csv
+from tqdm import tqdm
 
 # Define some constants
 SCREEN_WIDTH, SCREEN_HEIGHT = 800, 600
@@ -44,6 +46,8 @@ class Simulation:
         self.viscosity = viscosity
         self.gravity = gravity
         self.water_molecules = pygame.sprite.Group()
+        self.velocities = []
+        self.max_velocities = 100  # Maximum number of velocities to consider for the rolling average
 
     def get_pressure_force(self):
         return self.pressure_force
@@ -63,17 +67,28 @@ class Simulation:
     def set_gravity(self, gravity):
         self.gravity = gravity
 
-    def add_molecule(self):
-        x = random.uniform(SCREEN_WIDTH / 2 - PIPE_WIDTH / 2, SCREEN_WIDTH / 2 + PIPE_WIDTH / 2)
-        y = 0
-        water_molecule = WaterMolecule(x, y)
-        self.water_molecules.add(water_molecule)
+    def add_molecule(self, num_molecules=10):
+        for _ in range(num_molecules):
+            x = random.uniform(SCREEN_WIDTH / 2 - PIPE_WIDTH / 2, SCREEN_WIDTH / 2 + PIPE_WIDTH / 2)
+            y = 0
+            water_molecule = WaterMolecule(x, y)
+            self.water_molecules.add(water_molecule)
 
     def update_molecules(self):
         self.water_molecules.update(self.pressure_force, self.viscosity, self.gravity)
+        for molecule in self.water_molecules:
+            self.velocities.append(np.linalg.norm(molecule.speed))  # Add the magnitude of the velocity vector
+            if len(self.velocities) > self.max_velocities:
+                self.velocities.pop(0)  # Remove the oldest velocity if we've reached the maximum
 
     def draw_molecules(self, screen):
         self.water_molecules.draw(screen)
+
+    def get_average_velocity(self):
+        if self.velocities:
+            return sum(self.velocities) / len(self.velocities)
+        else:
+            return 0
 
 class GUI:
     def __init__(self, manager, simulation):
@@ -115,43 +130,84 @@ class GUI:
                 elif event.ui_element == self.gravity_slider:
                     self.simulation.set_gravity(self.gravity_slider.get_current_value())
 
-def main():
-    pygame.init()
+class DataCollector:
+    def __init__(self, pressure_bounds, viscosity_bounds, gravity_bounds):
+        self.pressure_bounds = np.arange(pressure_bounds[0], pressure_bounds[1], 0.05)
+        self.viscosity_bounds = np.arange(viscosity_bounds[0], viscosity_bounds[1], 0.05)
+        self.gravity_bounds = np.arange(gravity_bounds[0], gravity_bounds[1], 0.05)
 
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    clock = pygame.time.Clock()
-    manager = pygame_gui.UIManager((SCREEN_WIDTH, SCREEN_HEIGHT))
+    def run_pressure_simulations(self, viscosity, gravity):
+        for pressure in tqdm(self.pressure_bounds, desc="Pressure simulations"):
+            simulation = Simulation(pressure, viscosity, gravity)
+            self.run_simulation(simulation, pressure, viscosity, gravity)
 
-    simulation = Simulation(0.1, 0.99, 0.1)
-    gui = GUI(manager, simulation)
+    def run_viscosity_simulations(self, pressure, gravity):
+        for viscosity in tqdm(self.viscosity_bounds, desc="Viscosity simulations"):
+            simulation = Simulation(pressure, viscosity, gravity)
+            self.run_simulation(simulation, pressure, viscosity, gravity)
 
-    running = True
-    while running:
-        time_delta = clock.tick(60) / 1000.0
+    def run_gravity_simulations(self, pressure, viscosity):
+        for gravity in tqdm(self.gravity_bounds, desc="Gravity simulations"):
+            simulation = Simulation(pressure, viscosity, gravity)
+            self.run_simulation(simulation, pressure, viscosity, gravity)
 
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
-
-            manager.process_events(event)
-            gui.process_events(event)
-
-        manager.update(time_delta)
-
-        screen.fill(BACKGROUND_COLOR)
-        pygame.draw.line(screen, PIPE_COLOR, (SCREEN_WIDTH / 2 - PIPE_WIDTH / 2, 0), (SCREEN_WIDTH / 2 - PIPE_WIDTH / 2, SCREEN_HEIGHT), 2)
-        pygame.draw.line(screen, PIPE_COLOR, (SCREEN_WIDTH / 2 + PIPE_WIDTH / 2, 0), (SCREEN_WIDTH / 2 + PIPE_WIDTH / 2, SCREEN_HEIGHT), 2)
-
-        for _ in range(10):  # Add 10 molecules per frame
+    def run_simulation(self, simulation, pressure, viscosity, gravity):
+        for _ in tqdm(range(400), desc="Simulation steps"):  # Run the simulation for 1000 steps
             simulation.add_molecule()
-        simulation.update_molecules()
-        simulation.draw_molecules(screen)
+            simulation.update_molecules()
+        average_velocity = simulation.get_average_velocity()
+        self.write_to_csv(pressure, viscosity, gravity, average_velocity)
 
-        manager.draw_ui(screen)
+    def write_to_csv(self, pressure, viscosity, gravity, average_velocity):
+        with open('simulation_data.csv', 'a', newline='') as file:
+            writer = csv.writer(file)
+            writer.writerow([pressure, viscosity, gravity, average_velocity])
 
-        pygame.display.flip()
+class Main:
+    def __init__(self):
+        pass
 
-    pygame.quit()
+    def run_simulation_gui(self):
+        pygame.init()
+        self.screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+        self.clock = pygame.time.Clock()
+        self.manager = pygame_gui.UIManager((SCREEN_WIDTH, SCREEN_HEIGHT))
+        self.simulation = Simulation(0.1, 0.99, 0.1)
+        self.gui = GUI(self.manager, self.simulation)
+        running = True
+        while running:
+            time_delta = self.clock.tick(60) / 1000.0
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
+
+                self.manager.process_events(event)
+                self.gui.process_events(event)
+
+            self.manager.update(time_delta)
+
+            self.screen.fill(BACKGROUND_COLOR)
+            pygame.draw.line(self.screen, PIPE_COLOR, (SCREEN_WIDTH / 2 - PIPE_WIDTH / 2, 0), (SCREEN_WIDTH / 2 - PIPE_WIDTH / 2, SCREEN_HEIGHT), 2)
+            pygame.draw.line(self.screen, PIPE_COLOR, (SCREEN_WIDTH / 2 + PIPE_WIDTH / 2, 0), (SCREEN_WIDTH / 2 + PIPE_WIDTH / 2, SCREEN_HEIGHT), 2)
+
+            self.simulation.add_molecule()
+            self.simulation.update_molecules()
+            self.simulation.draw_molecules(self.screen)
+
+            self.manager.draw_ui(self.screen)
+
+            print(self.simulation.get_average_velocity())
+
+            pygame.display.flip()
+
+        pygame.quit()
+
+    def collect_data(self, pressure_bounds, viscosity_bounds, gravity_bounds):
+        data_collector = DataCollector(pressure_bounds, viscosity_bounds, gravity_bounds)
+        data_collector.run_gravity_simulations(0.1, 0.5)
 
 if __name__ == '__main__':
-    main()
+   cfd = Main()
+#    cfd.run_simulation_gui()
+   cfd.collect_data((0.1, 0.5), (0.9, 1.0), (0.1, 0.5))
